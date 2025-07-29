3884fe1cebbe6263df70a12d6b3e1b6d0e6a122e
bilaalmunir <bilaalmunir00@gmail.com>
2025-07-30 01:41:19 +0500

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -26,9 +26,8 @@ const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(
     return (
       <div
         ref={ref}
-        className={`h-full  transition-all duration-300 ease-in-out ${
-          isExpanded ? "w-64" : "w-28"
-        }`}
+        className={`h-full  transition-all duration-300 ease-in-out ${isExpanded ? "w-64 bg-[#D7DCF7]" : "w-28"
+          }`}
       >
         {/* Logo */}
         <div className="p-6">
@@ -38,26 +37,25 @@ const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(
         </div>
 
         {/* Navigation */}
-        <nav className="space-y-16 px-8 py-6">
+        <nav className="space-y-16 px-8 py-6 ">
           {menuItems.map((item, index) => (
             <Link key={index} href={item.href}>
               <button
                 onClick={onItemClick}
-                className={`flex items-center transition-all duration-200 ${
-                  isExpanded 
-                    ? "w-full space-x-3 p-4 rounded-xl text-left justify-start" 
+                className={`flex items-center transition-all duration-200 ${isExpanded
+                    ? "w-full space-x-3 p-4 rounded-xl text-left justify-start"
                     : "w-12 h-12 rounded-full p-0 justify-center mt-4"
-                } ${
-                  item.label === currentPage
-                    ? "bg-[#ea669f] bg-opacity-60 text-gray-800 shadow-sm "
+                  } ${item.label === currentPage
+                    ? isExpanded
+                      ? "bg-[#F1F1F1] bg-opacity-60 text-gray-800 shadow-sm"
+                      : "bg-[#D7DCF7] text-gray-800"
                     : "text-gray-700 hover:bg-white hover:bg-opacity-40"
-                }`}
+                  }`}
               >
                 <item.icon className="w-6 h-6 flex-shrink-0" />
                 <span
-                  className={`text-sm font-medium transition-all duration-300 ${
-                    isExpanded ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-2 w-0 overflow-hidden"
-                  }`}
+                  className={`text-sm font-medium transition-all duration-300 ${isExpanded ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-2 w-0 overflow-hidden"
+                    }`}
                 >
                   {item.label}
                 </span>
